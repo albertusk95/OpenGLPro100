dc0e38dae2bc7b31e5077fd92d5af8114fe50bf1
albertusk95 <albertusk15@gmail.com>
2016-06-23 23:01:54 +0700

    updated number of objects

--- OpenGLPro100/UserInteract.cpp
+++ OpenGLPro100/UserInteract.cpp
@@ -9,16 +9,16 @@ using namespace std;
 
 void getUserInput() {
 	cout << "OpenGLPro100" << endl;
-	cout << "--------------------------" << endl;
-	cout << "Albertus Kelvin - 13514100" << endl << endl;
+	cout << "---------------" << endl;
+	cout << "Albertus Kelvin" << endl << endl;
 
 	// input bagian SQUARE
 	cout << "[Square]" << endl;
 	cout << "Jumlah square (kotak): ";
 	cin >> numSquare;
 
-	if (numSquare < 0) {
-		cout << "Input jumlah tidak valid. Jumlah square dibuat default: 1" << endl;
+	if (numSquare <= 0) {
+		cout << "Input jumlah tidak valid (<=0). Jumlah square dibuat default: 1" << endl;
 		numSquare = 1;
 	}
 
@@ -34,8 +34,8 @@ void getUserInput() {
 	cout << "Jumlah circle (lingkaran): ";
 	cin >> numCircle;
 
-	if (numCircle < 0) {
-		cout << "Input jumlah tidak valid. Jumlah circle dibuat default: 1" << endl;
+	if (numCircle <= 0) {
+		cout << "Input jumlah tidak valid (<=0). Jumlah circle dibuat default: 1" << endl;
 		numCircle = 1;
 	}
 
@@ -51,8 +51,8 @@ void getUserInput() {
 	cout << "Jumlah octagon (segi-8): ";
 	cin >> numOctagon;
 
-	if (numOctagon < 0) {
-		cout << "Input jumlah tidak valid. Jumlah octagon dibuat default: 1" << endl;
+	if (numOctagon <= 0) {
+		cout << "Input jumlah tidak valid (<=0). Jumlah octagon dibuat default: 1" << endl;
 		numOctagon = 1;
 	}
 
@@ -70,7 +70,7 @@ void getUserInput() {
 }
 
 void setUserCircle(int idx) {
-	cout << "[Circle " << idx << "]" << endl;
+	cout << "[Circle " << idx << "]" << " - kode [0..1]" << endl;
 	cout << "R: ";
 	cin >> circles[idx]->R;
 	if (circles[idx]->R < 0 || circles[idx]->R > 1) {
@@ -99,7 +99,7 @@ void setUserCircle(int idx) {
 }
 
 void setUserSquare(int idx) {
-	cout << "[Square " << idx << "]" << endl;
+	cout << "[Square " << idx << "]" << " - kode [0..1]" << endl;
 	cout << "R: ";
 	cin >> squares[idx]->R;
 	if (squares[idx]->R < 0 || squares[idx]->R > 1) {
@@ -128,7 +128,7 @@ void setUserSquare(int idx) {
 }
 
 void setUserOctagon(int idx) {
-	cout << "[Octagon " << idx << "]" << endl;
+	cout << "[Octagon " << idx << "]" << " - kode [0..1]" << endl;
 	cout << "R: ";
 	cin >> octagons[idx]->R;
 	if (octagons[idx]->R < 0 || octagons[idx]->R > 1) {
